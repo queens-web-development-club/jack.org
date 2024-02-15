30392cdeabf057160557ccdc56af928127b3a9d8
Aqothy <aq0335@gmail.com>
2024-02-15 14:36:42 -0500

    navbar

---
+++ data/navData.js
@@ -0,0 +1,28 @@
+const navData = [
+  {
+    text: "HOME",
+    link: "/",
+  },
+  {
+    text: "ABOUT US",
+    link: "/about",
+  },
+  {
+    text: "RESOURCES",
+    link: "/resources",
+  },
+  {
+    text: "GET INVOLVED",
+    link: "/involve",
+  },
+  {
+    text: "BLOG",
+    link: "/blog",
+  },
+  {
+    text: "CONTACT",
+    link: "/contact",
+  },
+];
+
+export {navData}
