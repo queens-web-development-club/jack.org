0c31d026be61ed0caf990b2339de3abe551abce5
Aqothy <aq0335@gmail.com>
2024-03-31 15:33:50 -0400

    burger menu

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,9 +1,12 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { navData } from "@/data/navData";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
 export default function NavBar() {
-
   const logo = (
     <Link href="/" className="flex items-center">
       <Image
@@ -17,24 +20,36 @@ export default function NavBar() {
     </Link>
   );
 
+  const [open, setOpen] = useState(false);
+
   const links = (
-    <ul className="flex h-full items-center font-bold text-[1.3rem] text-white">
-      {navData.map((item, key) => (
-        <li
-          key={key}
-          className="hover:bg-orange-600 flex-1 h-full flex justify-center items-center"
-        >
-          <Link href={item.link}>{item.text}</Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <GiHamburgerMenu
+        className="text-[#F05814] text-[3rem]"
+        onClick={() => setOpen(!open)}
+      />
+      {open && (
+        <div className="w-[200px] h-fit absolute bg-[#202835] top-[10%]">
+          <ul className="flex flex-col justify-center font-bold text-[1.3rem] text-white">
+            {navData.map((item, key) => (
+              <li
+                key={key}
+                className="hover:bg-orange-600 flex-1 h-full flex justify-center items-center w-full p-[1rem]"
+              >
+                <Link href={item.link}>{item.text}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 
   return (
     <nav className="flex justify-center h-[100px] bg-[#202835]">
       <div className="flex w-[85%]">
         {logo}
-        <div className="flex-1 ml-[5rem]">{links}</div>
+        <div className="flex-1 ml-[5rem] flex items-center justify-end">{links}</div>
       </div>
     </nav>
   );
