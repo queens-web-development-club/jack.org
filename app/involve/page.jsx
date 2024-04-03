d7f3cf77887d06099682749ad024209a8e376f71
Adwait Srivastava <studyuse1702@gmail.com>
2024-04-02 21:03:16 -0400

    final commit to my branch

--- app/involve/page.jsx
+++ app/involve/page.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 import members from "../../public/membersimage.png"
 import summit from "../../public/summitimage.png"
 import events from "../../public/eventsimage.png"
-
+import movement from "../../public/movement.png"
 export default function Involve() {
   return (
-    <div className="flex bg-[#2F405B] lg:px-48 px-2 flex-col !gap-2 md:!gap-2 lg:gap-2 pt-[40px] mb-12 md:mb-16">
+    <div>
+    <div className="flex bg-[#2F405B] lg:px-48 px-2 flex-col !gap-2 md:!gap-2 lg:gap-2 pt-[40px] ">
     <div className="text-center lg:px-32 px-4">
       <p className="text-white font-bold md:text-4xl text-2xl">Want to join jack.org Queen’s Chapter?</p>
       <p className="flex text-white text-lg pt-8">Applications to join us are currently closed. Please come back when we post our hiring opportunities in MONTH. And look for other opportunities to get involved as a mental health activists above/below!</p>  
@@ -22,32 +23,59 @@ export default function Involve() {
       <p className="text-2xl font-bold pt-4">Apply to Join us Today!</p>
 
     </div>
-    <div className="flex lg:flex-row lg:px-16 flex-col gap-8 items-center text-white">
-  <div className="flex-grow font-bold md:text-2xl text-xl">Class 1
-  <Image
-            src={members}
-            alt=""
-            width={300}
-            className="lg:w-[75%] rounded-[20px]"
-          ></Image> 
-  
+    <div className="flex xl:flex-row px-4 xl:px-16 flex-col gap-8 items-center text-white">
+    <div className="flex-grow font-bold md:text-2xl text-xl">
+  <p className="w-fit mx-auto">Members</p>
+  <div className="flex flex-col items-center justify-center"> 
+    <Image
+      src={members}
+      alt=""
+      width={300}
+      className="xl:w-[75%] rounded-[20px]"
+    ></Image>
+    <button className="mt-4 px-6 py-2 bg-transparent text-white border-2 border-solid border-white rounded-lg hover:bg-[#F09E24] hover:border-2 hover:border-transparent">Apply Now</button>
   </div>
-  <div className="flex-grow font-bold md:text-2xl text-xl text-center">
-    <p className="w-fit mx-auto">Class 2</p>
-  <Image
-            src={summit}
-            alt=""
-            width={300}
-            className="lg:w-[75%] rounded-[20px]"
-          ></Image></div>
-  <div className="flex-grow font-bold md:text-2xl text-xl text-center">Class 3
-  <Image
-            src={events}
-            alt=""
-            width={300}
-            className="lg:w-[75%] rounded-[20px]"
-          ></Image></div>
+</div>
+<div className="flex-grow font-bold md:text-2xl text-xl">
+  <p className="w-fit mx-auto">Summit</p>
+  <div className="flex flex-col items-center justify-center"> 
+    <Image
+      src={summit}
+      alt=""
+      width={300}
+      className="xl:w-[75%] rounded-[20px]"
+    ></Image>
+    <button className="mt-4 px-6 py-2 bg-transparent text-white border-2 border-solid border-white rounded-lg hover:bg-[#F09E24] hover:border-2 hover:border-transparent">Apply Now</button>
+  </div>
+</div>
+<div className="flex-grow font-bold md:text-2xl text-xl">
+  <p className="w-fit mx-auto">Events</p>
+  <div className="flex flex-col items-center justify-center"> 
+    <Image
+      src={events}
+      alt=""
+      width={300}
+      className="xl:w-[75%] rounded-[20px]"
+    ></Image>
+    <button className="mt-4 px-6 py-2 bg-transparent text-white border-2 border-solid border-white rounded-lg hover:bg-[#F09E24] hover:border-2 hover:border-transparent">Apply Now</button>
+  </div>
+</div>
 </div>
     </div>
+    <div className="text-center bg-[#C5A272] lg:px-32 px-4">
+    <p className="text-white font-bold pt-32 md:text-6xl text-4xl">Want to do a talk or start a fundraiser?</p>
+    <p className="text-white text-4xl font-bold pt-8 "> Let us know more by contacting us </p>  
+    <div className="flex flex-col items-center justify-center"> 
+    <button className="mt-4 px-6 py-2 bg-transparent text-white border-2 border-solid border-white rounded-lg hover:bg-[#F09E24] hover:border-2 hover:border-transparent">Contact US</button>
+    </div>
+  </div>
+
+  <Image
+      src={movement}
+      alt=""
+      width={300}
+      className="w-[100%]"
+    ></Image>
+  </div>
   )
 }
