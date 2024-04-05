a06361732fd418d6df17021c48758b05838c3b72
jsong552 <james.song017@gmail.com>
2024-04-05 00:26:13 -0400

    resources card component done

--- app/resources/page.jsx
+++ app/resources/page.jsx
@@ -1,4 +1,5 @@
 import Banner from "@/components/resources/Banner";
+import ResourceCard from "@/components/resources/ResourceCard";
 import React from "react";
 
 export default function Resources() {
@@ -7,14 +8,51 @@ export default function Resources() {
       className="bg-[#202835]"
     >
       <h1
-        className="font-montserrat text-white text-5xl font-bold p-8 text-center"
+        className="font-montserrat text-white md:text-5xl text-3xl font-bold p-8 text-center"
       >
         Your Health is A Priority!</h1>
         <Banner 
           image="resources1.svg"
         />
+        <div className="flex flex-col">
+          <div className="flex flex-col sm:flex-row px-4 sm:justify-between">
+            <ResourceCard 
+              image="rcard1.svg"
+              text="Suicide crisis helpline"
+              contact="988"
+            />  
+            <ResourceCard 
+              image="rcard2.svg"
+              text="Immediate Danger or urgent medical support"
+              contact="911"
+            />
+            <ResourceCard 
+              image="rcard3.svg"
+              text="Mental Health Helpline (All ages)"
+              contact="+1-866-531-2600"
+            />  
+          </div>
+          <div className="flex flex-col sm:flex-row px-4 sm:justify-between">
+            <ResourceCard 
+              image="rcard4.svg"
+              text="Telephone Aid Line Kingston (TALK)"
+              contact="+1-613-544-1771"
+              subtitle="6pm - 2am"
+            />  
+            <ResourceCard 
+              image="rcard5.svg"
+              text="Crisis Mental Health Support at Student Wellness Services"
+              contact="+1-613-533-2506"
+              subtitle="1st floor Mitchell Hall, 69 Union Street"
+            />    
+            <ResourceCard 
+              image="rcard6.svg"
+              text="MindBeacon: Crisis Text Line"
+              contact="Text &quot;MindBeacon&quot; to 741741"
+            />  
+          </div>
+        </div>
 
-        {/* resources cards go here */}
 
         <Banner 
           image="resources2.svg"
--- components/resources/ResourceCard.jsx
+++ components/resources/ResourceCard.jsx
@@ -1,9 +1,71 @@
-import React from "react";
+"use client"
+import React, { useRef } from "react";
+import { motion, useAnimate } from "framer-motion";
 
-export default function ResourceCard({ image, text, contact }) {
+export default function ResourceCard({ image, text, contact, subtitle }) {
+  const [scope, animate] = useAnimate();
+  const textRef = useRef(null);
+  const contactRef = useRef(null);
+  const subtitleRef = useRef(null);
+
+  async function handleDivEnterAnimate() {
+    animate(scope.current, { opacity: 0.20 }, { duration: 0.25 });
+    animate(textRef.current, { opacity: 1.0 }, { duration: 0.25 });
+    animate(contactRef.current, { opacity: 1.0 }, { duration: 0.25 });
+    if (subtitle) {
+      animate(subtitleRef.current, { opacity: 1.0 }, { duration: 0.25 });
+    }
+  }
+
+  async function handleDivLeaveAnimate() {
+    animate(scope.current, { opacity: 1.0 }, { duration: 0.25 });
+    animate(textRef.current, { opacity: 0.0 }, { duration: 0.25 });
+    animate(contactRef.current, { opacity: 0.0 }, { duration: 0.25 });
+    if (subtitle) {
+      animate(subtitleRef.current, { opacity: 0.0 }, { duration: 0.25 });
+    }
+  }
+
   return (
-    <div>
-      
+    <div className="border-4 lg:m-8 md:m-4 sm:w-[35%] sm:m-2 lg:w-[28%] xs:w-1/2 w-4/5 m-4 mx-auto aspect-square bg-cover">
+      <div className="border-4 border-black h-full w-full relative">
+        <motion.div
+          className="w-full h-full bg-cover bg-center"
+          style={{ backgroundImage: `url(${image})` }}
+          ref={scope}
+          onMouseEnter={handleDivEnterAnimate}
+          onMouseLeave={handleDivLeaveAnimate}
+          initial={{ opacity: 1.0 }}
+          animate={{ opacity: 1.0 }}
+          transition={{ duration: 0.5 }}
+        ></motion.div>
+        <div 
+          className="w-4/5 lg:h-[60%] absolute lg:top-[20%] md:h-[70%] md:top-[15%] xs:h-[80%] xs:top-[10%] left-[10%] h-[70%] top-[15%] flex flex-col justify-around items-center"
+          style={{ pointerEvents: 'none' }}
+        >
+          <p
+            className="font-montserrat text-white text-center xl:text-2xl lg:text-base md:text-sm sm:text-xs text-base"
+            style={{ pointerEvents: 'none', opacity: 0 }}
+            ref={textRef}
+          >
+            {text}
+          </p>
+          <p
+            className="text-center text-white font-bold xl:text-3xl lg:text-lg md:text-base sm:text-sm text-xl"
+            style={{ pointerEvents: 'none', opacity: 0 }}
+            ref={contactRef}
+          >
+            {contact}
+          </p>
+          {subtitle && <p
+            className="text-center text-white font-light xl:text-xl lg:text-base md:text-sm sm:text-xs text-base"
+            style={{ pointerEvents: 'none', opacity: 0 }}
+            ref={subtitleRef}
+          >
+            {subtitle}
+          </p>}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
