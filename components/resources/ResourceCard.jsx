745959c1ad753a22f23b6c8a877931ea8b9fba36
Aqothy <aq0335@gmail.com>
2024-04-07 15:35:51 -0400

    fixed conditionally rendered use animate hook in build errors

--- components/resources/ResourceCard.jsx
+++ components/resources/ResourceCard.jsx
@@ -1,14 +1,17 @@
 "use client"
+
 import React, { useRef } from "react";
 import { motion, useAnimate } from "framer-motion";
 import Link from "next/link";
 
-export default function ResourceCard({ transparent, image, text, contact, subtitle, link }) {
-  if (transparent) {
-    return (
-      <div className="lg:m-8 md:m-4 sm:w-[35%] sm:m-2 lg:w-[28%] xs:w-1/2 w-4/5 m-4 mx-auto aspect-[416/396] bg-cover"></div>
-    )
-  }
+export default function ResourceCard({
+  transparent,
+  image,
+  text,
+  contact,
+  subtitle,
+  link,
+}) {
   // image and text fields required to use this component. transparent, contact, subtitle, and link are optional.
   const [scope, animate] = useAnimate();
   const textRef = useRef(null);
@@ -17,7 +20,7 @@ export default function ResourceCard({ transparent, image, text, contact, subtit
   const linkRef = useRef(null);
 
   async function handleDivEnterAnimate() {
-    animate(scope.current, { opacity: 0.20 }, { duration: 0.25 });
+    animate(scope.current, { opacity: 0.2 }, { duration: 0.25 });
     animate(textRef.current, { opacity: 1.0 }, { duration: 0.25 });
     if (contact) {
       animate(contactRef.current, { opacity: 1.0 }, { duration: 0.25 });
@@ -26,7 +29,7 @@ export default function ResourceCard({ transparent, image, text, contact, subtit
       animate(subtitleRef.current, { opacity: 1.0 }, { duration: 0.25 });
     }
     if (link) {
-      animate(linkRef.current, { opacity: 1.0}, { duration: 0.25 });
+      animate(linkRef.current, { opacity: 1.0 }, { duration: 0.25 });
     }
   }
 
@@ -45,7 +48,7 @@ export default function ResourceCard({ transparent, image, text, contact, subtit
   }
 
   async function handleLinkHover() {
-    animate(scope.current, { opacity: 0.20 }, { duration: 0 });
+    animate(scope.current, { opacity: 0.2 }, { duration: 0 });
     animate(textRef.current, { opacity: 1.0 }, { duration: 0 });
     if (contact) {
       animate(contactRef.current, { opacity: 1.0 }, { duration: 0 });
@@ -57,11 +60,13 @@ export default function ResourceCard({ transparent, image, text, contact, subtit
   }
 
   return (
-    <motion.div 
-      className="border-4 lg:m-8 md:m-4 sm:w-[35%] sm:m-2 lg:w-[28%] xs:w-1/2 w-4/5 m-4 mx-auto aspect-[416/396] bg-cover"
+    <motion.div
+      className={`border-4 lg:m-8 md:m-4 sm:w-[35%] sm:m-2 lg:w-[28%] xs:w-1/2 w-4/5 m-4 mx-auto aspect-[416/396] bg-cover ${
+        transparent ? "bg-transparent" : ""
+      }`}
       whileHover={{
         scale: 1.05,
-        transition: { duration: 0.25 }
+        transition: { duration: 0.25 },
       }}
     >
       <div className="border-4 border-black h-full w-full relative">
@@ -75,43 +80,48 @@ export default function ResourceCard({ transparent, image, text, contact, subtit
           animate={{ opacity: 1.0 }}
           transition={{ duration: 0.5 }}
         ></motion.div>
-        <div 
+        <div
           className="w-4/5 lg:h-[60%] absolute lg:top-[20%] md:h-[70%] md:top-[15%] xs:h-[80%] xs:top-[10%] left-[10%] h-[70%] top-[15%] flex flex-col justify-around items-center"
-          style={{ pointerEvents: 'none' }}
+          style={{ pointerEvents: "none" }}
         >
           <p
             className="font-montserrat text-white text-center xl:text-2xl lg:text-base md:text-sm sm:text-xs text-base"
-            style={{ pointerEvents: 'none', opacity: 0 }}
+            style={{ pointerEvents: "none", opacity: transparent ? 0 : 1 }}
             ref={textRef}
           >
             {text}
           </p>
-          {contact && <p
-            className="text-center text-white font-bold xl:text-3xl lg:text-lg md:text-base sm:text-sm text-xl"
-            style={{ pointerEvents: 'none', opacity: 0 }}
-            ref={contactRef}
-          >
-            {contact}
-          </p>}
-          {subtitle && <p
-            className="text-center text-white font-light xl:text-xl lg:text-base md:text-sm sm:text-xs text-base"
-            style={{ pointerEvents: 'none', opacity: 0 }}
-            ref={subtitleRef}
-          >
-            {subtitle}
-          </p>}
+          {contact && (
+            <p
+              className="text-center text-white font-bold xl:text-3xl lg:text-lg md:text-base sm:text-sm text-xl"
+              style={{ pointerEvents: "none", opacity: transparent ? 0 : 1 }}
+              ref={contactRef}
+            >
+              {contact}
+            </p>
+          )}
+          {subtitle && (
+            <p
+              className="text-center text-white font-light xl:text-xl lg:text-base md:text-sm sm:text-xs text-base"
+              style={{ pointerEvents: "none", opacity: transparent ? 0 : 1 }}
+              ref={subtitleRef}
+            >
+              {subtitle}
+            </p>
+          )}
           {link && (
-          <Link 
-            target="_blank" 
-            rel="noopener noreferrer" 
-            href={link.href} 
-            ref={linkRef}
-            onMouseEnter={() => handleLinkHover()}
-            style={{ pointerEvents: 'auto', opacity: 0 }}
-            className="hover:underline text-center text-white font-semibold xl:text-3xl lg:text-lg md:text-base sm:text-sm text-xl"
-          >
-            {link.text}
-          </Link>)}
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              href={link.href}
+              ref={linkRef}
+              onMouseEnter={() => handleLinkHover()}
+              style={{ pointerEvents: "auto", opacity: transparent ? 0 : 1 }}
+              className="hover:underline text-center text-white font-semibold xl:text-3xl lg:text-lg md:text-base sm:text-sm text-xl"
+            >
+              {link.text}
+            </Link>
+          )}
         </div>
       </div>
     </motion.div>
